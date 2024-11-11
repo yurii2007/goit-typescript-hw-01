5a9b8025868ca60c9390c86ee8d22a369f4c8b7d
yurii2007 <yurash862@gmail.com>
2024-11-11 20:05:26 +0200

    transaction

--- index.ts
+++ index.ts
@@ -1,15 +1,43 @@
-import "dotenv";
+import 'dotenv/config';
+import { getKeypairFromEnvironment } from '@solana-developers/helpers';
 import {
+  clusterApiUrl,
   Connection,
   LAMPORTS_PER_SOL,
   PublicKey,
-  clusterApiUrl,
-} from "@solana/web3.js";
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction,
+} from '@solana/web3.js';
 
-const connection = new Connection(clusterApiUrl("devnet"));
-const address = new PublicKey("CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN");
-const balance = await connection.getBalance(address);
+const TRANSACTION_AMOUNT = 0.1 * LAMPORTS_PER_SOL;
 
-const balanceInSOL = balance / LAMPORTS_PER_SOL;
+const senderKeypair = getKeypairFromEnvironment('SOLANA_KEY');
+const recipientKey = process.argv[2] || null;
 
-console.log(`balance = ${balanceInSOL} SOL`);
+if (!recipientKey) {
+  console.error('Please provide a recipient key');
+  process.exit(1);
+}
+
+const recipientAddress = new PublicKey(recipientKey);
+
+const connection = new Connection(clusterApiUrl('devnet'));
+
+const transaction = new Transaction();
+
+const sendSOLInstruction = SystemProgram.transfer({
+  fromPubkey: senderKeypair.publicKey,
+  toPubkey: recipientAddress,
+  lamports: TRANSACTION_AMOUNT,
+});
+
+transaction.add(sendSOLInstruction);
+
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+  senderKeypair,
+]);
+
+console.log(signature);
+
+process.exit(0);
