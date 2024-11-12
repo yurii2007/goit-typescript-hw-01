c6a28a69b5831cab1af05435399eb5c7a3033aed
yurii2007 <yurash862@gmail.com>
2024-11-12 20:48:11 +0200

    lesson 5 lab

--- components/providers/WalletContextProvider.tsx
+++ components/providers/WalletContextProvider.tsx
@@ -3,14 +3,19 @@ import {
   WalletProvider,
 } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { FC, ReactNode } from 'react';
+import { clusterApiUrl } from '@solana/web3.js';
+import { FC, ReactNode, useMemo } from 'react';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 export const WalletContextProvider: FC<{ chidlren: ReactNode }> = ({
   children,
 }) => {
+  const endpoint = clusterApiUrl('devnet');
+  const wallets = useMemo(() => [], []);
+
   return (
-    <ConnectionProvider endpoint="">
-      <WalletProvider wallets={[]}>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,25 @@
-import { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import { AppBar } from '../components/AppBar'
-import Head from 'next/head'
-import { PingButton } from '../components/PingButton'
+import { NextPage } from 'next';
+import styles from '../styles/Home.module.css';
+import { AppBar } from '../components/AppBar';
+import Head from 'next/head';
+import { PingButton } from '../components/PingButton';
+import { WalletContextProvider } from '../components/providers';
 
 const Home: NextPage = (props) => {
-
   return (
     <div className={styles.App}>
       <Head>
         <title>Wallet-Adapter Example</title>
-        <meta
-          name="description"
-          content="Wallet-Adapter Example"
-        />
+        <meta name="description" content="Wallet-Adapter Example" />
       </Head>
+      <WalletContextProvider>
         <AppBar />
         <div className={styles.AppBody}>
-          <PingButton/>
+          <PingButton />
         </div>
+      </WalletContextProvider>
     </div>
   );
-}
+};
 
 export default Home;
