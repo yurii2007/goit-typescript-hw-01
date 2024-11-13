f0a7f2a07c14d716a2a3b1d846c771372455962e
yurii2007 <yurash862@gmail.com>
2024-11-13 16:50:45 +0200

    removed client code

--- src/components/shared/Balance/BalanceMessage.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-
-const BalanceMessage: FC<{ balance: number }> = ({ balance }) => {
-  return <h3>Balance: {balance.toFixed(6)}SOL</h3>;
-};
-
-export default BalanceMessage;
