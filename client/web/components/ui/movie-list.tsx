faa1d735ef42979af84396bca0375dd4a65b0ddf
yurii2007 <yurash862@gmail.com>
2024-11-22 18:49:39 +0200

    lab 2 steps

--- client/web/components/ui/movie-list.tsx
+++ client/web/components/ui/movie-list.tsx
@@ -4,6 +4,7 @@ import { Movie } from '@/models/movie-model';
 import { useConnection } from '@solana/wallet-adapter-react';
 import { PublicKey } from '@solana/web3.js';
 import toast from 'react-hot-toast';
+import { MovieCoordinator } from '@/coordinator/MovieCoordinator';
 
 const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN';
 
@@ -21,77 +22,45 @@ const DATA_LENGTH = 100;
 
 export const MovieList: FC = () => {
   const { connection } = useConnection();
-  const [movies, setMovies] = useState<Movie[]>([]); // Specify the type here
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     if (!connection) return;
 
     (async () => {
       try {
-        // const accounts = await connection.getProgramAccounts(
-        //   new PublicKey(MOVIE_REVIEW_PROGRAM_ID),
-        //   // { dataSlice: { length: 10, offset: 1 } }
-        // );
-        const accountsWithData = await connection.getProgramAccounts(
-          new PublicKey(MOVIE_REVIEW_PROGRAM_ID),
-          // INITIALIZED field(1 for bool) + RATING field(8 for u8) + 4 for the storing string size; Length is 40 because of expected length is 40
-          { dataSlice: { offset: DATA_OFFSET, length: DATA_LENGTH } }
-        );
-
-        const accounts = Array.from(accountsWithData.slice(0, 20));
-        accounts.sort((a, b) => {
-          try {
-            // Check if buffers are long enough to avoid out-of-bounds access
-            const lengthA = a.account.data.readUInt32LE(0);
-            const lengthB = b.account.data.readUInt32LE(0);
-
-            if (
-              a.account.data.length < STRING_LENGTH_SPACE + lengthA ||
-              b.account.data.length < STRING_LENGTH_SPACE + lengthB
-            ) {
-              throw new Error('Buffer length is insufficient');
-            }
-
-            const dataA = a.account.data.subarray(
-              STRING_LENGTH_SPACE,
-              STRING_LENGTH_SPACE + lengthA
-            );
-            const dataB = b.account.data.subarray(
-              STRING_LENGTH_SPACE,
-              STRING_LENGTH_SPACE + lengthB
-            );
-            console.log(dataA.compare(dataB));
-
-            return dataB.compare(dataA);
-          } catch (error) {
-            console.error('Error sorting accounts: ', error);
-            return 0; // Default sort order in case of error
-          }
-        });
-
-        const accountKeys = accounts.map(({ pubkey }) => pubkey);
-
-        const accountInfos = await connection.getMultipleAccountsInfo(
-          accountKeys
-        );
-        const movies = accountInfos
-          .map((account) => Movie.deserialize(account?.data))
-          .filter((movie) => movie !== null);
-
+        const movies = await MovieCoordinator.fetchPage(connection, page, 10);
         setMovies(movies);
       } catch (error: any) {
-        console.error(error);
-        toast.error(error.message);
+        console.error('Fetch movies error', error);
       }
-      
     })();
-  }, [setMovies, connection]);
+  }, [setMovies, connection, page]);
 
   return (
     <div className="py-5 flex flex-col w-fullitems-center justify-center">
       {movies.map((movie, i) => (
         <Card key={i} movie={movie} />
       ))}
+      <div className="flex justify-between mt-4">
+        {page > 0 && (
+          <button
+            onClick={() => setPage((page) => page - 1)}
+            className="px-6 py-2 bg-gray-300 text-black font-semibold rounded"
+          >
+            Previous
+          </button>
+        )}
+        {movies.length % 10 === 0 && (
+          <button
+            onClick={() => setPage((page) => page + 1)}
+            className="px-6 py-2 bg-gray-300 text-black font-semibold rounded"
+          >
+            Next
+          </button>
+        )}
+      </div>
     </div>
   );
 };
---
+++ client/web/coordinator/MovieCoordinator.ts
@@ -0,0 +1,54 @@
+import { Movie } from '@/models/movie-model';
+import { Connection, PublicKey } from '@solana/web3.js';
+
+const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN';
+
+export class MovieCoordinator {
+  static accounts: PublicKey[] = [];
+  static MOVIE_PROGRAM_ID = new PublicKey(MOVIE_REVIEW_PROGRAM_ID);
+
+  static async prefetchAccounts(connection: Connection) {
+    try {
+      const accounts = await connection.getProgramAccounts(
+        MovieCoordinator.MOVIE_PROGRAM_ID,
+        { dataSlice: { length: 0, offset: 0 } }
+      );
+
+      this.accounts = accounts.map(({ pubkey }) => pubkey);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  static async fetchPage(
+    connection: Connection,
+    page: number,
+    limit: number,
+    reload = false
+  ): Promise<Array<Movie>> {
+    if (this.accounts.length === 0 || reload) {
+      await this.prefetchAccounts(connection);
+    }
+
+    const paginatedKeys = this.accounts.slice(
+      page * limit,
+      page * limit + limit
+    );
+
+    if (paginatedKeys.length === 0) return [];
+
+    const accounts = await connection.getMultipleAccountsInfo(paginatedKeys);
+
+    const movies = accounts.reduce<Movie[]>((acc, account) => {
+      try {
+        const movie = Movie.deserialize(account?.data);
+        return movie ? acc.concat([movie]) : acc;
+      } catch (error) {
+        console.error('Serialization error', error);
+        return acc;
+      }
+    }, []);
+
+    return movies;
+  }
+}
