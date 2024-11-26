ff427b6b2a432dab83aa7d19986f5d977b523787
yurii2007 <yurash862@gmail.com>
2024-11-26 14:09:46 +0200

    nft lesson

--- client/index.ts
+++ client/index.ts
@@ -1,16 +1,19 @@
 import {
   createNft,
+  fetchMetadataFromSeeds,
   mplTokenMetadata,
+  updateV1,
 } from '@metaplex-foundation/mpl-token-metadata';
 import {
   createGenericFile,
   keypairIdentity,
   percentAmount,
+  publicKey,
 } from '@metaplex-foundation/umi';
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
 import { mockStorage } from '@metaplex-foundation/umi-storage-mock';
 import { getKeypairFromFile } from '@solana-developers/helpers';
-import { clusterApiUrl } from '@solana/web3.js';
+import { clusterApiUrl, PublicKey } from '@solana/web3.js';
 import fs from 'node:fs/promises';
 
 const umi = createUmi(clusterApiUrl('devnet'));
@@ -41,4 +44,21 @@ const { signature, result } = await createNft(umi, {
   uri,
   updateAuthority: umi.identity.publicKey,
   sellerFeeBasisPoints: percentAmount(0),
+  // isCollection: true,
 }).sendAndConfirm(umi, { send: { commitment: 'finalized' } });
+
+const nft = await fetchMetadataFromSeeds(umi, {
+  mint: publicKey(signature.toString()),
+});
+
+await updateV1(umi, {
+  mint,
+  authority: umi.identity,
+  data: {
+    ...nft, 
+    sellerFeeBasisPoints: 0,
+    name: "Updated",
+  },
+  primarySaleHappened: true,
+  isMutable: true,
+}).sendAndConfirm(umi)
